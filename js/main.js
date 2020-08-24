87d49b2283e3d2fc40d4d29d69aee3b010ae4c09
Nicole Parra <nicoleparrae@gmail.com>
2020-08-23 23:41:01 -0400

    add & adjust flex container and update buttons

--- js/main.js
+++ js/main.js
@@ -1,20 +1,23 @@
 function getData() {
-    var inner = $('.inner')
-    var input = $("#search").val()
-    //request 
-    var xhr = $.get('http://api.giphy.com/v1/gifs/search?q=' + input + '+&api_key=URG7FMX93pwFlDetgwBBhsCyG3X9dEPO&limit=30');
-   
-    //promise 
-    xhr.done(function (response) {
-        console.log("success got data", response);
-        var jiffs = response.data
+  var inner = $('.inner')
+  var input = $("#search").val()
+  //request 
+  var xhr = $.get('http://api.giphy.com/v1/gifs/search?q=' + input + '+&api_key=URG7FMX93pwFlDetgwBBhsCyG3X9dEPO&limit=30');
 
-        inner.empty()
-        
-        for (i in jiffs) {
-          inner.append("<img src='" + jiffs[i].images.original.url + "'/>")
+  //promise 
+  xhr.done(function (response) {
+    console.log("success got data", response);
+    var jiffs = response.data
 
-        }
+    inner.empty()
+
+    for (i in jiffs) {
+      inner.append("<img src='"+jiffs[i].images.fixed_width.url+"' style='height:350px; width:350px '/>")
+
     }
-    );
+  }
+  );
+
+
+
 }
