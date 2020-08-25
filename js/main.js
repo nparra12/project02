b068ff4dd8f4bef0336e8cf8dc67f421533b4e4e
Nicole Parra <nicoleparrae@gmail.com>
2020-08-25 16:00:51 -0400

    commit to master

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ function getData() {
   var inner = $('.inner')
   var input = $("#search").val()
   //request 
-  var xhr = $.get('http://api.giphy.com/v1/gifs/search?q=' + input + '+&api_key=URG7FMX93pwFlDetgwBBhsCyG3X9dEPO&limit=30');
+  var xhr = $.get('https://api.giphy.com/v1/gifs/search?q=' + input + '+&api_key=URG7FMX93pwFlDetgwBBhsCyG3X9dEPO&limit=30');
 
   //promise 
   xhr.done(function (response) {
